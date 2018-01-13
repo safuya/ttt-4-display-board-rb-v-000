4912679b04fc54b0f6f985bad1cdee2c42ffe358
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 17:24:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,8 @@ end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  seperator
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  seperator
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
